03627f9f9e714cc686c3b519d972baeabcf525db
Devin Rucks <blindnives@gmail.com>
2020-08-20 20:03:05 -0400

    Successfully imported svg images into react component

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../scss/weather.scss';
 import { observer } from 'mobx-react';
-
+import Snowy from '../svgs/snowy-4.svg'
 
 
 
@@ -9,7 +9,7 @@ const Weather = observer(() => {
 
      return (
           <section id="weather-container">
-               Weather
+               <img style={{ height: 160, width: 160, border: "1px red solid" }} src={Snowy} alt="snowy" />
           </section>
      )
 
